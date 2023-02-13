f8bc3f1627363eb800b5a6295dd0abf5433d4af6
ergasap <ernestogaspar2000@gmail.com>
2023-02-13 13:01:16 +0100

    Heat map

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 import datosMapa from '../services/overview.js'
 import datosTabla from '../services/kpi.js'
+import datosPie from '../services/kpi.js'
 import datosCountry from '../services/mapData.js'
 
 
@@ -28,6 +29,12 @@ router.get('/datos-map', function (req, res) {
     res.setHeader('Content-type', 'text/json')
     var jsonMapa = datosMapa()
 	res.send(jsonMapa)
+})
+
+router.get('/datos-pie', function (req, res) {
+    res.setHeader('Content-type', 'text/json')
+    var jsonPie = datosPie() //implementar funcion
+	res.send(jsonPie)
 })
 
 router.get('/datos-country/:name', datosCountry)
--- src/services/kpi.js
+++ src/services/kpi.js
@@ -50,3 +50,40 @@ export default function datosTabla(){
 
     return json
 }
+
+export function datosPie(){
+    
+    var workbook = xlsx.readFile("src/data/kpis.xlsx")
+    var datos = {
+        "1OCV":[],
+        "2OCV":[],
+        "3OCV":[],
+        "1OCP":[],
+        "2OCP":[],
+        "3OCP":[],
+    } //json de arrays (un array por cada grafica)
+    
+    //lectura de datos
+    let worksheet = workbook.Sheets[workbook.SheetNames[4]];
+    var valores = ["M", "N", "O"];
+    var categorias = "P";
+    var array = []
+
+          
+    //recorre las filas y columnas de la tabla y va insertando los datos en un json
+    valores.forEach(e => {
+        for(let j=25; j<32; j++){
+            var json = {
+                category: "",
+                value: ""
+            }
+            var category = worksheet[`${e}${j}`].v
+            var value = worksheet[`${e}${j}`].v
+            console.log(category)
+            //jsonSheet.sheet.push(jsonAux)
+        }
+    })
+
+    
+    //return json
+}
